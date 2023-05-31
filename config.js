5622fcf1a210805aa0f67dc5f6cfe5dcd6bb6511
Yash Sharma <ysfcghh@gmail.com>
2023-05-31 12:36:56 +0530

    endpoint updated

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    END_POINT: 'https://chatnow-c646.onrender.com/api'
+    END_POINT: 'https://chatnow-49sw.onrender.com/api'
 }
 
 export default config
--- utils/socket.js
+++ utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io('https://chatnow-c646.onrender.com', {
+const socket = io('https://chatnow-49sw.onrender.com', {
     autoConnect: false
 })
 
